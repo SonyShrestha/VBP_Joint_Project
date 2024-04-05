0bc9446c8cbb67d387c63ca7fdc8c5afd12bb64e
sony <sony.sth8@gmail.com>
2024-04-05 13:28:57 +0200

    Selenium version issue resolved

--- landing_zone/collectors/eat_by_date/eat_by_date.py
+++ landing_zone/collectors/eat_by_date/eat_by_date.py
@@ -179,13 +179,19 @@ if __name__ == "__main__":
     contact_url = "https://eatbydate.com/contact//"
 
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_driver_path = ChromeDriverManager().install()
-    service = Service(chrome_driver_path)
-    driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+    chrome_options.add_argument("--headless")  # Add other options as needed
+
+    # Initialize the Chrome WebDriver with webdriver_manager
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_driver_path = ChromeDriverManager().install()
+    # service = Service(chrome_driver_path)
+    # driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
     eat_by_date_json = scrap_eat_by_date(driver,url)
 
     with open(os.path.join(raw_data_dir,'eat_by_date.json'), 'w') as json_file:
