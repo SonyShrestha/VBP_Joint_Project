55fcfb0ec6b85f3686103dc6e0a56ccb36e970eb
1-ARIjitS <arijits19@iiserb.ac.in>
2024-04-06 23:49:31 +0200

    minip bucket updated

--- minio_bucket.py
+++ minio_bucket.py
@@ -2,6 +2,20 @@ import docker
 import time
 from minio import Minio
 from minio.error import S3Error
+import configparser
+import os
+
+# Get the path to the parent parent directory
+config_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir, os.pardir))
+print(config_dir)
+
+# Specify the path to config file
+config_file_path = os.path.join(config_dir, "config.ini")
+
+# reading the config file
+config = configparser.ConfigParser()
+config.read(config_file_path)
+raw_data_folder_path= config['COMMON']['raw_data_dir']
 
 def main():
     # Create a client with the MinIO server playground, its access key
@@ -13,7 +27,7 @@ def main():
     )
 
     # The file to upload, change this path if needed
-    source_file = "D:/BDMA/UPC/BDM/P1/VBP_Joint_Project/data/raw/ApprovedFood.json"
+    # source_file = "D:/BDMA/UPC/BDM/P1/VBP_Joint_Project/data/raw/ApprovedFood.json"
 
     # The destination bucket and filename on the MinIO server
     bucket_name = "spicybytes-bucket"
@@ -28,13 +42,16 @@ def main():
         print("Bucket", bucket_name, "already exists")
 
     # Upload the file, renaming it in the process
-    client.fput_object(
-        bucket_name, destination_file, source_file,
-    )
-    print(
-        source_file, "successfully uploaded as object",
-        destination_file, "to bucket", bucket_name,
-    )
+    for filename in os.listdir(raw_data_folder_path):
+        filepath= os.path.join(raw_data_folder_path, filename)
+        destination_file= filename
+        client.fput_object(
+            bucket_name, destination_file, filepath,
+        )
+        print(
+            filepath, "successfully uploaded as object",
+            destination_file, "to bucket", bucket_name,
+        )
 
 if __name__ == "__main__":
     try:
