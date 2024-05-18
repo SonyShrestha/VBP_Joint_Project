959782f6c41eff77739ae4253ba56d308386ed17
sony <sony.sth8@gmail.com>
2024-05-18 08:39:00 +0200

    Dummy feature 1 test

--- Website/pages/feature_1.py
+++ Website/pages/feature_1.py
@@ -1,10 +1,20 @@
 import streamlit as st
+import pandas as pd
 
+# Title of the app
+st.title('Customer Purchase Details')
 
-def show_feature1():
-    st.header("Feature 1")
-    st.write(
-        """
-        This is the SpicyBytes Website FEATURE 1
-        """
-    )
+# Specify the path to the local Parquet file
+parquet_file_path = 'C:\\UPC\\BDM\\Project\\VBP_Joint_Project\\data\\formatted_zone\\purchases_nearing_expiry'
+
+try:
+    # Read the parquet file into a DataFrame
+    df = pd.read_parquet(parquet_file_path)
+    
+    # Display the DataFrame
+    st.write(df)
+    
+except FileNotFoundError:
+    st.error(f"File not found: {parquet_file_path}")
+except Exception as e:
+    st.error(f"An error occurred: {e}")
