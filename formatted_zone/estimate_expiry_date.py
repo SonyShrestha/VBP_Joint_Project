97cd9c2ee5210c987278d94d994597c6fba65ded
sony <sony.sth8@gmail.com>
2024-05-22 00:11:09 +0200

    Dimension table for DW

--- formatted_zone/estimate_expiry_date.py
+++ formatted_zone/estimate_expiry_date.py
@@ -73,26 +73,6 @@ def count_tokens(s1, s2):
     return len(s1_tokens.intersection(s2_tokens))
 
 
-@udf(IntegerType())
-def spacy_match_score(spacy_filter, spacy_threshold, s1, s2):
-    s1 = nlp(s1)
-    s2 = nlp(s2)
-
-    s1_verbs = " ".join([token.lemma_ for token in s1 if token.pos_ == spacy_filter])
-    s2_verbs = " ".join([token.lemma_ for token in s1 if token.pos_ == spacy_filter])
-
-    doc1 = nlp(s1_verbs)
-    doc2 = nlp(s2_verbs)
-
-    # Calculate similarity
-    score = doc1.similarity(doc2)
-
-    if score < spacy_threshold:
-        score = 0
-    return score
-
-
-
 if __name__ == "__main__":
     gcs_config = config["GCS"]["credentials_path"]
     raw_bucket_name = config["GCS"]["raw_bucket_name"]
@@ -102,14 +82,13 @@ if __name__ == "__main__":
     fuzzy_threshold = config_json["product_matching"]["fuzzy_matching"]["threshold"]
 
     spark = SparkSession.builder \
-        .appName("Estimate Expiry Date") \
-        .config("spark.driver.host", "127.0.0.1") \
-        .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
-        .config("spark.hadoop.fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS") \
-        .config("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
-        .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", gcs_config) \
-        .getOrCreate()
-    
+    .appName("GCS Files Read") \
+    .config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.2") \
+    .config("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
+    .config("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS") \
+    .config("google.cloud.auth.service.account.json.keyfile", "C:\\UPC\\BDM\\Project\\VBP_Joint_Project\\gcs_config.json") \
+    .getOrCreate()
+
     # Specify the path to the Parquet file
     cust_purachase = f'gs://{formatted_bucket_name}/customer_purchase*'
 
@@ -125,6 +104,8 @@ if __name__ == "__main__":
 
     customer_purachase_df = cust_purachase_df.join(cust_email_df, 'customer_id', 'inner')
     customer_purachase_df = customer_purachase_df.select("id","customer_id","customer_name","email_id","product_name","unit_price","quantity","purchase_date")
+            
+    # customer_purachase_df = customer_purachase_df.filter(customer_purachase_df["product_name"] == "Natureland Organics Chana Besan  (500 g)")
 
     customer_purachase_df = customer_purachase_df.withColumn("original_product_name", customer_purachase_df["product_name"])
 
@@ -147,9 +128,10 @@ if __name__ == "__main__":
 
     filtered_df = filtered_df.withColumn("score", fuzzy_match_score(lit(fuzzy_score_calc_method), lit(fuzzy_threshold), filtered_df["product_name"], filtered_df["product_in_avg_expiry_file"]))
 
-    filtered_df = filtered_df.filter(filtered_df.score != 0)
 
     filtered_df = filtered_df.withColumn("token_count", count_tokens(filtered_df["product_name"], filtered_df["product_in_avg_expiry_file"]))
+
+    # filtered_df = filtered_df.withColumn("score", spacy_match_score(filtered_df["product_name"], filtered_df["product_in_avg_expiry_file"]))
 
     windowSpec = Window.partitionBy("id") \
                   .orderBy(filtered_df["score"].desc(), filtered_df["token_count"].desc())
@@ -158,10 +140,14 @@ if __name__ == "__main__":
     df_with_rn = filtered_df.withColumn("row_number", row_number().over(windowSpec))
 
     # Filter rows where row number is 1 (which corresponds to the row with the maximum fuzzy score for each product)
-    df_with_rn = df_with_rn.filter(df_with_rn["row_number"] == 1).drop("row_number", "product_name", "token_count")
+    df_with_rn = df_with_rn.filter(((df_with_rn["score"] != 0) & (df_with_rn["row_number"] == 1))).drop("row_number", "product_name", "token_count")
     df_with_rn = df_with_rn.withColumnRenamed("original_product_name", "product_name")
 
     df_with_rn = df_with_rn.withColumn("expected_expiry_date", expr("date_add(purchase_date, cast(ceil(avg_expiry_days/2) AS INT))"))
 
-    # df_with_rn.write.mode('overwrite').parquet("./data/formatted_zone/purchases_nearing_expiry")
+    # debug_df = df_with_rn.select("product_name","product_in_avg_expiry_file","avg_expiry_days")
+    # debug_df.write.csv('./data/formatted_zone/expiry_date_accuracy')
+
+    cnt = df_with_rn.count()
+    print(cnt)
     df_with_rn.write.mode('overwrite').parquet(f'gs://{formatted_bucket_name}/purchases_nearing_expiry')
